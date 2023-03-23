0a2983b951356a35b9cfeb1f9001b0e3197e4182
Thoraya <thorayaelsharawy@gmail.com>
2023-03-23 21:08:28 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      Hello
+    </div>
+  );
+}
+
+export default App;
