9e10bb90db7a31bb305b0e01e534062ae6a45057
Thoraya <thorayaelsharawy@gmail.com>
2023-03-23 21:21:31 +0000

    Route to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      Hello
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
   );
 }
 
