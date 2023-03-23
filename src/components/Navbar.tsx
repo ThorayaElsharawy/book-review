64d787a6458b33adf2c3203f57078a30731a1f13
Thoraya <thorayaelsharawy@gmail.com>
2023-03-23 22:12:57 +0000

    Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,96 @@
+import { useState } from "react";
+
 export function Navbar() {
-    return (
-        <div>
-            
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <nav className="relative bg-white shadow dark:bg-gray-800">
+        <div className="container px-6 py-4 mx-auto">
+          <div className="lg:flex lg:items-center lg:justify-between">
+            <div>
+              <h2 className="text-lg">Books Review</h2>
+            </div>
+            <div className="w-1/3">
+              <form>
+                <label
+                  htmlFor="default-search"
+                  className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                >
+                  Search
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <input
+                    type="text"
+                    id="default-search"
+                    className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 
+                    focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Search Books, Authors..."
+                    required
+                  />
+
+                  <button
+                    type="submit"
+                    className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Search
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="flex">
+              <button
+                type="button"
+                className="mx-5 rounded-full text-blue-800 px-1 "
+              >
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                  />
+                </svg>
+              </button>
+              <button
+                type="button"
+                className="flex rounded-full bg-gray-200 text-sm"
+                id="user-menu-button"
+                aria-expanded="false"
+                aria-haspopup="true"
+              >
+                <img
+                  className="h-9 w-9 rounded-full"
+                  src="src/assets/user.jpg"
+                  alt=""
+                />
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
